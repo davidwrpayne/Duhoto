0c47e535fc3614d42f1d4ea220d7c74078d6bcdf
David Payne <david.payne@bigcommerce.com>
2016-02-11 09:52:41 -0800

    adding match window. doesn't work yet

--- src/MatchWindow.scala
+++ src/MatchWindow.scala
@@ -1,5 +1,6 @@
 import java.awt.Color
 import java.io.File
+import javax.swing.ImageIcon
 
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
@@ -10,17 +11,45 @@ import scala.swing._
   */
 case class MatchWindow(matches: mutable.HashMap[String, ArrayBuffer[File]]) {
 
+
+
   def start = {
     val panel = new FlowPanel() {
       background = Color.RED
+
     }
-//    panel.contents += new Button("hello")
+    panel.contents += new Button("hello")
+
+    matches.values.foreach({ matchArray =>
+      val t = createPanelForMatch(matchArray)
+      panel.contents += t
+    })
+
     val top = new Frame() {
       contents = panel
       size = new Dimension(400, 400)
 
     }
+
+
     top.pack()
+
     top.visible = true
+
+  }
+
+
+  def createPanelForMatch(matchArray: ArrayBuffer[File]):Component = {
+    val panel = new ListView[Label]()
+    val som: ArrayBuffer[Label] =for(file <- matchArray) yield {
+      val imageLabel = new Label() {
+        icon = new ImageIcon(file.getPath()  )
+      }
+      imageLabel
+    }
+
+    panel.listData = som
+    panel
+
   }
 }
